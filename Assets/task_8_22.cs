fc2695918857f4b373ae16b4d213e3d93d4e9319
Ahmed Asafrah <211033@ppu.edu.ps>
2024-11-02 00:39:05 +0200

    new changes

--- Assets/task_8_22.cs
+++ Assets/task_8_22.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class task_8_22 : MonoBehaviour
+public class task_8 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        string[] words = { "Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
+        string sentence = "";
+
+        int i = 0;
+        while (i < 7)
+        {
+
+            int random = Random.Range(0, words.Length);
+            sentence += words[random] + " ";
+            i++;
+        }
+        Debug.Log(sentence);
+
     }
 }
